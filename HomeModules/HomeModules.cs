4f46d723feb9c1dbe241acafcf7844eb7a9ff047
Cassie Musolf and Nicole Sanders <cassie+nicole@>
2017-02-16 16:15:49 -0800

    add nancy to make webpage work.

---
+++ HomeModules/HomeModules.cs
@@ -0,0 +1,22 @@
+using Nancy;
+using ScrabbleApp.Objects;
+using System.Collections.Generic;
+
+namespace ScrabbleApp
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Post["/results"] = _ => {
+        string word = Request.Form["word"];
+        Scrabble newWord = new Scrabble(word);
+        int result = newWord.CalculateScore();
+        return View["index.cshtml", result];
+      };
+    }
+  }
+}
